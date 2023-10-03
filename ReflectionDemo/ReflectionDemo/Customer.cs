0eb797d101d6b5a5d5ea9cc7011acf2b3c57f457
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-10-03 10:23:02 +0530

    [Jashu]Add: Reflection Demo.

---
+++ ReflectionDemo/ReflectionDemo/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    internal class Customer
+    {
+        public int a, b;
+        public int id { get; set; }
+        public string name { get; set; }
+        public Customer()
+        {
+            id = 1;
+            name = "jashu";
+        }
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+        public void Demo()
+        {
+
+        }
+        public void Demo(int id)
+        {
+
+        }
+    }
+}
